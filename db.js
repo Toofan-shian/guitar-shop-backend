c22bf634f914e05065cbf4befac4c75fa07515c6
Toofan shian <toofan.shian@gmail.com>
2023-03-07 20:21:15 +0330

    changes in front end

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const {MongoClient} = require('mongodb')
 
 let dbConnection
 let dbUri = 'mongodb+srv://toofan:test123456@cluster0.bpt6m7c.mongodb.net/?retryWrites=true&w=majority'
-
+let local = 'mongodb://127.0.0.1:27017'
 module.exports = {
   connectToDb: (cb) => {
     MongoClient.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true})
